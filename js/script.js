c8d4d3513cc1f44c276b5220e298d3dc0387bca5
tschuulez <72871233+tschuulez@users.noreply.github.com>
2021-01-23 18:54:22 +0100

    avergage

--- js/script.js
+++ js/script.js
@@ -22,9 +22,18 @@ window.addEventListener("load", function () {
     console.log("so viele buttons wurden hinzugefügt " + buttons.length);
 });
 var score = 0;
+//Funktion um wieder auf die Startseite zu gelanden - 3 Buttons/ 3 Spielstärken erscheinden wieder 
+function playAgain() {
+    location.reload();
+}
+document.querySelector(".fa-redo").addEventListener("click", function () {
+    playAgain();
+});
 var cards = [];
 console.log("im Moment sind so viele Karten in deinem Array " + cards.length);
 window.addEventListener("load", function () {
+    //diese Funtkion soll ausgeführt werden beim Klicken des EASY Buttons - 8 Karten/ divs werden erzeugt und das
+    //MemoryBoard/ die Flexbox 
     function EASY() {
         var memoryBoard = document.createElement("div");
         memoryBoard.id = "memoryBoard1";
@@ -60,17 +69,124 @@ window.addEventListener("load", function () {
         }, {
             cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
             cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
         });
     }
     document.getElementById("button1").addEventListener("click", function () {
         buttonBox.style.visibility = "hidden";
         EASY();
-        console.log(cards.length);
+        console.log("So viele Karten wurden hinzugefügt " + cards.length);
+        buttons.length = 0; //Schwierigkeit-buttons verschwinden / erzeugte Buttons verschwinden wieder
+        console.log("die Buttons sind jetzt wieder verschwunden -> " + buttons.length);
+    });
+    //document.getElementById(".cardbackground").addEventListener("click", function(): void {
+    //});
+});
+window.addEventListener("load", function () {
+    //diese Funtkion soll ausgeführt werden beim Klicken des EASY Buttons - 8 Karten/ divs werden erzeugt und das
+    //MemoryBoard/ die Flexbox 
+    function AVERAGE() {
+        var memoryBoard2 = document.createElement("div");
+        memoryBoard2.id = "memoryBoard2";
+        var card1 = document.createElement("div");
+        card1.className = "cardbackground";
+        var card2 = document.createElement("div");
+        card2.className = "cardbackground";
+        var card3 = document.createElement("div");
+        card3.className = "cardbackground";
+        var card4 = document.createElement("div");
+        card4.className = "cardbackground";
+        var card5 = document.createElement("div");
+        card5.className = "cardbackground";
+        var card6 = document.createElement("div");
+        card6.className = "cardbackground";
+        var card7 = document.createElement("div");
+        card7.className = "cardbackground";
+        var card8 = document.createElement("div");
+        card8.className = "cardbackground";
+        var card9 = document.createElement("div");
+        card9.className = "cardbackground";
+        var card10 = document.createElement("div");
+        card10.className = "cardbackground";
+        var card11 = document.createElement("div");
+        card11.className = "cardbackground";
+        var card12 = document.createElement("div");
+        card12.className = "cardbackground";
+        var card13 = document.createElement("div");
+        card13.className = "cardbackground";
+        var card14 = document.createElement("div");
+        card14.className = "cardbackground";
+        var card15 = document.createElement("div");
+        card15.className = "cardbackground";
+        var card16 = document.createElement("div");
+        card16.className = "cardbackground";
+        document.querySelector(".box").appendChild(memoryBoard2);
+        document.querySelector("#memoryBoard2").appendChild(card1);
+        document.querySelector("#memoryBoard2").appendChild(card2);
+        document.querySelector("#memoryBoard2").appendChild(card3);
+        document.querySelector("#memoryBoard2").appendChild(card4);
+        document.querySelector("#memoryBoard2").appendChild(card5);
+        document.querySelector("#memoryBoard2").appendChild(card6);
+        document.querySelector("#memoryBoard2").appendChild(card7);
+        document.querySelector("#memoryBoard2").appendChild(card8);
+        document.querySelector("#memoryBoard2").appendChild(card9);
+        document.querySelector("#memoryBoard2").appendChild(card10);
+        document.querySelector("#memoryBoard2").appendChild(card11);
+        document.querySelector("#memoryBoard2").appendChild(card12);
+        document.querySelector("#memoryBoard2").appendChild(card13);
+        document.querySelector("#memoryBoard2").appendChild(card14);
+        document.querySelector("#memoryBoard2").appendChild(card15);
+        document.querySelector("#memoryBoard2").appendChild(card16);
+        //pushe karten in den array cards
+        cards.push({
+            cardstext: "DOM-Manipilation bezeichnet...",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        }, {
+            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+            cardflipped: false
+        });
+    }
+    document.getElementById("button2").addEventListener("click", function () {
+        buttonBox.style.visibility = "hidden";
+        AVERAGE();
+        console.log("So viele Karten wurden hinzugefügt " + cards.length);
         buttons.length = 0; //Schwierigkeit-buttons verschwinden / erzeugte Buttons verschwinden wieder
         console.log("die Buttons sind jetzt wieder verschwunden -> " + buttons.length);
     });
 });
-var button1 = document.querySelector(".button1");
-var button2 = document.querySelector(".button1");
-var button3 = document.querySelector(".button1");
 //# sourceMappingURL=script.js.map
--- js/script.ts
+++ js/script.ts
@@ -35,6 +35,15 @@ window.addEventListener("load", function(): void {
 
 let score: number = 0;
 
+//Funktion um wieder auf die Startseite zu gelanden - 3 Buttons/ 3 Spielstärken erscheinden wieder 
+function playAgain(): void {
+    location.reload();
+}
+
+document.querySelector(".fa-redo").addEventListener("click", function(): void {
+    playAgain();
+});
+
 //Interface für meine Karten-Objekte
 interface Card {
     cardstext: string;
@@ -46,12 +55,9 @@ let cards: Card [] = [];
 console.log("im Moment sind so viele Karten in deinem Array " + cards.length);
 
 window.addEventListener("load", function(): void {
-
-    
-
+    //diese Funtkion soll ausgeführt werden beim Klicken des EASY Buttons - 8 Karten/ divs werden erzeugt und das
+    //MemoryBoard/ die Flexbox 
     function EASY(): void {
-
-        
 
         let memoryBoard: HTMLDivElement = document.createElement("div");
         memoryBoard.id = "memoryBoard1";
@@ -86,19 +92,156 @@ window.addEventListener("load", function(): void {
         //pushe karten in den array cards
         cards.push({
            cardstext: "DOM-Manipilation bezeichnet...",
-            cardflipped: false
+           cardflipped: false
         },
                    {
            cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
            cardflipped: false
-       }
+       },
+                   {
+        cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+        cardflipped: false
+        },
+                   {
+        cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+        cardflipped: false
+        },
+                   {
+        cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+        cardflipped: false
+        },
+                   {
+        cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+        cardflipped: false
+        },
+                   {
+        cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+        cardflipped: false
+        },
+                   {
+        cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+        cardflipped: false
+        }
+
         );
     }
     document.getElementById("button1").addEventListener("click", function(): void {
         
         buttonBox.style.visibility = "hidden";
         EASY();
-        console.log(cards.length);
+        console.log("So viele Karten wurden hinzugefügt " + cards.length);
+        buttons.length = 0; //Schwierigkeit-buttons verschwinden / erzeugte Buttons verschwinden wieder
+        console.log("die Buttons sind jetzt wieder verschwunden -> " + buttons.length);
+    }); 
+
+    //document.getElementById(".cardbackground").addEventListener("click", function(): void {
+        
+    
+
+    //});
+
+});
+
+window.addEventListener("load", function(): void {
+    //diese Funtkion soll ausgeführt werden beim Klicken des EASY Buttons - 8 Karten/ divs werden erzeugt und das
+    //MemoryBoard/ die Flexbox 
+    function AVERAGE(): void {
+
+        let memoryBoard2: HTMLDivElement = document.createElement("div");
+        memoryBoard2.id = "memoryBoard2";
+        let card1: HTMLDivElement = document.createElement("div");
+        card1.className = "cardbackground";
+        let card2: HTMLDivElement = document.createElement("div");
+        card2.className = "cardbackground";
+        let card3: HTMLDivElement = document.createElement("div");
+        card3.className = "cardbackground";
+        let card4: HTMLDivElement = document.createElement("div");
+        card4.className = "cardbackground";
+        let card5: HTMLDivElement = document.createElement("div");
+        card5.className = "cardbackground";
+        let card6: HTMLDivElement = document.createElement("div");
+        card6.className = "cardbackground";
+        let card7: HTMLDivElement = document.createElement("div");
+        card7.className = "cardbackground";
+        let card8: HTMLDivElement = document.createElement("div");
+        card8.className = "cardbackground";
+        let card9: HTMLDivElement = document.createElement("div");
+        card9.className = "cardbackground";
+        let card10: HTMLDivElement = document.createElement("div");
+        card10.className = "cardbackground";
+        let card11: HTMLDivElement = document.createElement("div");
+        card11.className = "cardbackground";
+        let card12: HTMLDivElement = document.createElement("div");
+        card12.className = "cardbackground";
+        let card13: HTMLDivElement = document.createElement("div");
+        card13.className = "cardbackground";
+        let card14: HTMLDivElement = document.createElement("div");
+        card14.className = "cardbackground";
+        let card15: HTMLDivElement = document.createElement("div");
+        card15.className = "cardbackground";
+        let card16: HTMLDivElement = document.createElement("div");
+        card16.className = "cardbackground";
+    
+    
+        document.querySelector(".box").appendChild(memoryBoard2);
+    
+        document.querySelector("#memoryBoard2").appendChild(card1);
+        document.querySelector("#memoryBoard2").appendChild(card2);
+        document.querySelector("#memoryBoard2").appendChild(card3);
+        document.querySelector("#memoryBoard2").appendChild(card4);
+        document.querySelector("#memoryBoard2").appendChild(card5);
+        document.querySelector("#memoryBoard2").appendChild(card6);
+        document.querySelector("#memoryBoard2").appendChild(card7);
+        document.querySelector("#memoryBoard2").appendChild(card8);
+        document.querySelector("#memoryBoard2").appendChild(card9);
+        document.querySelector("#memoryBoard2").appendChild(card10);
+        document.querySelector("#memoryBoard2").appendChild(card11);
+        document.querySelector("#memoryBoard2").appendChild(card12);
+        document.querySelector("#memoryBoard2").appendChild(card13);
+        document.querySelector("#memoryBoard2").appendChild(card14);
+        document.querySelector("#memoryBoard2").appendChild(card15);
+        document.querySelector("#memoryBoard2").appendChild(card16);
+        //pushe karten in den array cards
+        cards.push({
+           cardstext: "DOM-Manipilation bezeichnet...",
+           cardflipped: false
+        },
+                   {
+           cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+           cardflipped: false
+       },
+                   {
+           cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+           cardflipped: false
+        },
+                   {
+           cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+           cardflipped: false
+        },
+                   {
+           cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+           cardflipped: false
+        },
+                   {
+           cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+           cardflipped: false
+        },
+                   {
+           cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+           cardflipped: false
+        },
+                   {
+           cardstext: "das Manipulieren von Inhalten zur Laufzeit des Browsers.",
+           cardflipped: false
+        }
+
+        );
+    }
+    document.getElementById("button2").addEventListener("click", function(): void {
+        
+        buttonBox.style.visibility = "hidden";
+        AVERAGE();
+        console.log("So viele Karten wurden hinzugefügt " + cards.length);
         buttons.length = 0; //Schwierigkeit-buttons verschwinden / erzeugte Buttons verschwinden wieder
         console.log("die Buttons sind jetzt wieder verschwunden -> " + buttons.length);
     }); 
@@ -106,9 +249,6 @@ window.addEventListener("load", function(): void {
 });
 
 
-let button1: HTMLDivElement = document.querySelector(".button1");
-let button2: HTMLDivElement = document.querySelector(".button1");
-let button3: HTMLDivElement = document.querySelector(".button1");
 
 
 
